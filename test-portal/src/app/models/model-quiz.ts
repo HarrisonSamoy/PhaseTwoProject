43c1a1452593c8bcaea8b3b59ed40b7c9b559a43
HarrisonSamoy <hsamoy98@gmail.com>
2020-11-08 22:00:01 -0500

    added questionaire form and question saving

--- test-portal/src/app/models/model-quiz.ts
+++ test-portal/src/app/models/model-quiz.ts
@@ -2,13 +2,17 @@ export class Quiz {
 
     private questionList: Question[] = [];
 
-    public addQuestion(q:string, one:string, two:string, three:string, correct:string):void {
+    public addQuestion(q:string, options:string[], correct:number):void {
         this.questionList.push(
-            new Question( this.questionList.length+1, q, one, two, three, correct ));
+            new Question( this.questionList.length+1, q, options, correct ));
     }
     
     public getQuestion(index:number): Question {
         return this.questionList.find(q => q.id === index);
+    }
+
+    public setQuestionGuess(qNum:number, optNum:number) {
+        this.questionList[qNum - 1].guess = optNum;
     }
 
     public length():number {
@@ -17,7 +21,9 @@ export class Quiz {
 }
 
 export class Question {
+
+    public guess:number = -1;
+
     constructor(public id: number, public question: string,
-        public wrongOne: string, public wrongTwo: string, 
-        public wrongThree: string, public correct: string) {}
+        public options:string[], public correct:number) {}
 }
--- test-portal/src/app/question/question.component.ts
+++ test-portal/src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEventPattern } from 'rxjs';
 import { Question, Quiz } from '../models/model-quiz';
 
 @Component({
@@ -6,6 +7,7 @@ import { Question, Quiz } from '../models/model-quiz';
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
+
 export class QuestionComponent implements OnInit {
 
   qNum:number = 1;
@@ -15,8 +17,9 @@ export class QuestionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.quiz.addQuestion("What is your name?", "ME", "WE", "US", "YOU");
-    this.quiz.addQuestion("What is your name2?", "ME2", "WE2", "US2", "YOU2");
+    this.quiz.addQuestion("What is your name?", ["ME", "WE", "US", "Right"], 3);
+    this.quiz.addQuestion("What is your name2?", ["ME2", "Right2", "US2", "YOU2"], 1);
+    this.quiz.addQuestion("What is your name3?", ["ME3", "Right3", "US3", "YOU3"], 1);
 
     this.setCurrentQuestion();
   }
@@ -33,9 +36,16 @@ export class QuestionComponent implements OnInit {
       this.setCurrentQuestion();
     }
   }
+  save(form: any) {
+    console.log("Question #" + this.qNum + ": answer = " + form.questionaire);
+    if (form.questionaire != "") {
+      this.quiz.setQuestionGuess(this.qNum, eval(form.questionaire));
+    }
+  }
   
   private setCurrentQuestion() {
     this.qCurrent = this.quiz.getQuestion(this.qNum);
+    console.log("Current guess is..." + this.qCurrent.guess);
   }
 
 }
